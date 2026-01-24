4e9623045a79f130eac9e2a0311d560fb215abba
igorsatsyuk <igor.satsyuk2@gmail.com>
2026-01-24 08:25:06 +0200

    Integrate `ObservationRestTemplateCustomizer` for enhanced RestTemplate instrumentation.

--- src/main/java/lt/satsyuk/config/RestTemplateConfig.java
+++ src/main/java/lt/satsyuk/config/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package lt.satsyuk.config;
 
+import org.springframework.boot.actuate.metrics.web.client.ObservationRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder, ObservationRestTemplateCustomizer customizer) {
+        return customizer.customize(builder).build();
     }
 }
